5b8b64484c73b5edcda8a54210711d0c33df8b6c
Wander Luiz da Silva Flores <wanderluiz2312@gmail.com>
2025-01-24 21:25:18 -0400

    Update index.js

--- UC 8 - Desenvolver aplicações mobile/Aplicativos/Blog/api_blog/index.js
+++ UC 8 - Desenvolver aplicações mobile/Aplicativos/Blog/api_blog/index.js
@@ -14,6 +14,41 @@ const db = mysql.createConnection({
     database: 'blog'
 });
 
+//CRUD USUARIO
+app.get('/usuarios', (req, res) => {
+    db.query('SELECT * FROM usuarios', (erro, results) => {
+        if (erro) throw erro;
+        res.json(results);
+    });
+});
+
+app.post('/usuarios', (req, res) => {
+    const {nome, email} = req.body;
+    db.query('INSERT INTO usuarios (nome, email) VALUES (?,?)', [nome, email], (erro, result) => {
+        if(erro) throw erro;
+        res.json({id_usuarios: result.insertId, nome, email});
+    });
+});
+
+app.put('/usuarios/:id_usuario', (req, res) => {
+    const { id_usuario } = req.params;
+    const { nome, email } = req.body;
+    db.query('UPDATE usuarios SET nome = ?, email = ? WHERE id_usuario = ?', [nome, email, id_usuario], (erro) => {
+        if (erro) throw erro;
+        res.json({ message: 'Usuário atualizado com sucesso.' });
+    });
+});
+
+app.delete('/usuarios/:id_usuario', (req, res) => {
+     const { id_usuario } = req.params;
+    db.query('DELETE FROM usuarios WHERE id_usuario = ?', [id_usuario], (erro) => {
+        if (erro) throw erro;
+        res.json({ message: 'Usuário deletado com sucesso.' });
+    });
+});
+
+//POST CRUD
+
 db.connect((erro) =>{
     if (erro){
         console.error('Erro ao conectar ao banco de dados:', erro);
