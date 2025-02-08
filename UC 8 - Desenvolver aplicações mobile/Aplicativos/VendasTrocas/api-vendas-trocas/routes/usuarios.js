01aadba6c376657a4ef69647e9a86b29bb39029b
Wander Luiz da Silva Flores <wanderluiz2312@gmail.com>
2025-02-07 20:00:07 -0400

    Update

--- UC 8 - Desenvolver aplicações mobile/Aplicativos/VendasTrocas/api-vendas-trocas/routes/usuarios.js
+++ UC 8 - Desenvolver aplicações mobile/Aplicativos/VendasTrocas/api-vendas-trocas/routes/usuarios.js
@@ -2,4 +2,14 @@ const express = require('express');
 const router = express.Router();
 const connection = require('../database');
 
+router.post('/', (req, res) => {
+    const { nome, email, senha, telefone, localizacao, foto_perfil } = req.body;
+    const query = 
+    'INSERT INTO usuarios (nome, email, senha, telefone, localizacao, foto_perfil) VALUES (?, ?, ?, ?, ?, ?)';
+    connection.query(query, [nome, email, senha, telefone, localizacao, foto_perfil], (err, results) => {
+        if (err) return res.status(500).json({error: err.message });
+        res.status(201).json({ id: results.insertId});
+    });
+});
+
 module.exports = router;
