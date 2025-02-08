fcc2dc06a7edf4e721679c4f94c6bdb0f854c5bf
Wander Luiz da Silva Flores <wanderluiz2312@gmail.com>
2025-02-07 20:49:16 -0400

    Update CadastroUsuario.js

--- UC 8 - Desenvolver aplicações mobile/Aplicativos/VendasTrocas/app-vendas-trocas/src/screens/CadastroUsuario.js
+++ UC 8 - Desenvolver aplicações mobile/Aplicativos/VendasTrocas/app-vendas-trocas/src/screens/CadastroUsuario.js
@@ -1,5 +1,5 @@
 import React, { useState} from 'react';
-import { View, Text, TextInput, Button, Alert, StyleSheet } from 'react-native';
+import { View, Text, TextInput, Button, Alert, StyleSheet, ActivityIndicator } from 'react-native';
 import api from '../services/api';
 
 const CadastroUsuario = () => {
@@ -8,8 +8,28 @@ const CadastroUsuario = () => {
     const [senha, setSenha] = useState('');
     const [telefone, setTelefone] = useState('');
     const [localizacao, setLocalizacao] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const validarCampos = () => {
+        if (!nome || !email || !senha || !telefone || !localizacao) {
+            Alert.alert('Erro', 'Todos os campos são obrigatórios.');
+            return false;
+        }
+        if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)){
+            Alert.alert('Erro', 'Por favor, insira um e-mail válido.');
+            return false;
+        }
+        if (senha.length < 6){
+            Alert.alert('Erro', 'A senha deve ter pelo menos 6 caracteres.');
+            return false;
+        }
+        return true
+    };
 
     const handleCadastro = async () => {
+        if (!validarCampos()) return;
+        setLoading(true);
+
         try{
             const response = await api.post('/usuarios', {
                 nome,
@@ -20,9 +40,12 @@ const CadastroUsuario = () => {
             });
             Alert.alert('Sucesso', 'Usuário cadastrado com sucesso!');
             console.log(response.data);
+            navigation.navigate('Login');
         }catch (error) {
             Alert.alert('Erro', 'Não foi possivel cadastrar o usuário');
             console.error(error);
+        }finally{
+            setLoading(false);
         }
     };
 
@@ -68,8 +91,11 @@ const CadastroUsuario = () => {
             onChangeText = {setLocalizacao}
             />
 
-            <Button title = 'Cadastrar' onPress = {handleCadastro} />
-
+            {loading ? (
+                <ActivityIndicator size = 'large' color = '#0000ff' />
+            ) : (
+                <Button title = 'Cadastrar' onPress = {handleCadastro} />    
+            )}
         </View>
     );
 };
