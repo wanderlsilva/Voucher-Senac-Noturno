0f3885c2b8441b6067e2c1a24067a4d670dab77c
Wander Luiz da Silva Flores <wanderluiz2312@gmail.com>
2025-02-07 21:15:10 -0400

    Update

--- UC 8 - Desenvolver aplicações mobile/Aplicativos/VendasTrocas/app-vendas-trocas/App.js
+++ UC 8 - Desenvolver aplicações mobile/Aplicativos/VendasTrocas/app-vendas-trocas/App.js
@@ -1,12 +1,27 @@
 import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from "expo-status-bar";
 import CadastroUsuario from "./src/screens/CadastroUsuario";
+import Login from "./src/screens/Login";
+
+const Stack = createStackNavigator();
 
 export default function App () {
   return (
-    <>
-      <StatusBar style = "auto" />
-      <CadastroUsuario />
-    </>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName = "CadastroUsuario">
+        <Stack.Screen
+        name = "CadastroUsuario"
+        component = {CadastroUsuario}
+        options = {{ title: 'Cadastro' }}
+        />
+        <Stack.Screen
+        name = "Login"
+        component = {Login}
+        options = {{ title: 'Login' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
---
+++ UC 8 - Desenvolver aplicações mobile/Aplicativos/VendasTrocas/app-vendas-trocas/src/screens/Login.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { View, Text, Button, StyleSheet  } from "react-native-web";
+
+const Login = ({ navigation }) => {
+    return (
+        <View style = {styles.container}>
+            <Text style = {styles.title}> Tela de Login </Text>
+            <Button
+            title = 'Ir para Cadastro'
+            onPress={() => navigation.navigate('CadastroUsuario')}
+            />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 16,
+    },
+});
+
+export default Login;
